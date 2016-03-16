b6c9b990474482791ca579adc3a96430cff7e288
Mac Chapman <mac@veryhappythings.co.uk>
2016-03-16 13:23:21 +0000

    Initial commit :sunrise:

---
+++ README.md
@@ -0,0 +1,15 @@
+A basic discord bot for Dota 2 pickup games.
+
+Currently this just waits for 10 players to sign in, then shuffles them up
+and splits them into two teams. There's a lot more to do before it's good,
+just wanted to get what I'd done online.
+
+To use:
+
+* Get your bot a username/password by signing up to discord for them, and
+  connecting in to any servers you want them to be on.
+* Install python 3.5.
+* pip install -r requirements.txt
+* Put your bot's username and password into config.json (see config.json.example)
+* Run gatherbot.py. It will connect to all the channels in all the servers you
+  put them in when you set up the username.
---
+++ lib/bot.py
@@ -0,0 +1,50 @@
+import logging
+import re
+import discord
+
+
+logger = logging.getLogger(__name__)
+
+
+class Bot:
+    def __init__(self, username, password):
+        self.client = discord.Client()
+        self.actions = {}
+        self.username = username
+        self.password = password
+
+        @self.client.event
+        async def on_ready():
+            logger.info('Logged in as')
+            logger.info(self.client.user.name)
+            logger.info(self.client.user.id)
+            logger.info('------')
+
+        @self.client.event
+        async def on_message(message):
+            if message.author != self.client.user:
+                logger.info('Message received [{0}]: "{1}"'.format(message.channel, message.content))
+                for regex, fn in self.actions.values():
+                    match = re.match(regex, message.content)
+                    if match:
+                        try:
+                            await fn(message, *match.groups())
+                        except Exception as e:
+                            logger.exception(e)
+                            await self.say(message.channel, 'Something went wrong with that command.')
+                        break
+
+    def run(self):
+        self.client.run(self.username, self.password)
+
+    def action(self, regex):
+        def decorator(f):
+            logger.info('Registering action {0}'.format(regex))
+            if regex in self.actions:
+                logger.info('Overwriting regex {0}'.format(regex))
+            self.actions[regex] = (re.compile(regex, re.IGNORECASE), f)
+            return f
+        return decorator
+
+    async def say(self, channel, message):
+        await self.client.send_message(channel, message)
