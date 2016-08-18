a154a6c6c5f65d2ddd1d67ed0156c197e308352d
Mac Chapman <mac@veryhappythings.co.uk>
2016-08-18 08:28:56 +0100

    Move command tests to correct file

--- test/test_commands.py
+++ test/test_commands.py
@@ -1,20 +1,9 @@
-import asyncio
 import unittest
 from unittest import mock
 from gather import commands
 
 
-def async_test(f):
-    # http://stackoverflow.com/a/23036785/304210
-    def wrapper(*args, **kwargs):
-        coro = asyncio.coroutine(f)
-        future = coro(*args, **kwargs)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(future)
-    return wrapper
-
-
-class TestGatherBot(unittest.TestCase):
+class TestCommands(unittest.TestCase):
     def test_help(self):
         self.assertEqual(
             ['Hello!'],
